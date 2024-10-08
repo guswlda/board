a4c26cd4d22aba074e9b3ae43257c83393b09d73
guswlda <guswl71945@gmail.com>
2024-10-08 16:31:21 +0900

    Update deleteBoard.js

--- controllers/deleteBoard.js
+++ controllers/deleteBoard.js
@@ -4,7 +4,7 @@ exports.deleteBoards = async (req, res) => {
   const post_id = req.params.post_id;
 
    try {
-    const result = await pool.query('DELETE FROM board WHERE post_id = 1;;');
+    const result = await database.query('DELETE FROM board WHERE post_id = 1;;');
     return res.status(200).json({ message: 'Board Put Successfully' });
   } catch (error) {
     return res.status(500).json({ message: 'Update Completed Fail' + error });
