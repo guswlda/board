01fd6c1c17f30ab1c527606233be10d244226392
guswlda <guswl71945@gmail.com>
2024-10-08 16:39:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,6 @@ const express = require('express'); // express 모듈 사용
 const app = express(); // express 모듈을 app 변수 할당
 app.use(express.json()); // req.body를 JSON으로 파싱
 
-// app.get('/', (req, res) => {
-//   res.send('hello world');
-// });
-
 app.use(require('./routes/getRoutes')); // getRoutes 사용
 app.use(require('./routes/postRoutes')); // postRoutes 사용
 app.use(require('./routes/putRoutes')); // updateRoutes 사용
