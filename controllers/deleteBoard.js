cd773d3eccdea4d4f55c5bec0ade0544be98ad82
guswlda <guswl71945@gmail.com>
2024-10-08 16:30:20 +0900

    Update deleteBoard.js

--- controllers/deleteBoard.js
+++ controllers/deleteBoard.js
@@ -1,8 +1,12 @@
-// app.delete('/delete_board', async (req, res) => {
-//   try {
-//     const result = await pool.query('DELETE FROM board WHERE post_id = 1;;');
-//     return res.status(200).json({ message: 'Board Put Successfully' });
-//   } catch (error) {
-//     return res.status(500).json({ message: 'Update Completed Fail' + error });
-//   }
-// });
+const database = require('../database/database'); // database 모듈 불러오기
+
+exports.deleteBoards = async (req, res) => {
+  const post_id = req.params.post_id;
+
+   try {
+    const result = await pool.query('DELETE FROM board WHERE post_id = 1;;');
+    return res.status(200).json({ message: 'Board Put Successfully' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Update Completed Fail' + error });
+  }
+};
