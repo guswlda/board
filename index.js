e814a7b2fffedf02c6c48c8d1566dbb39285eeab
guswlda <guswl71945@gmail.com>
2024-10-08 16:20:02 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,15 +7,6 @@ app.use(express.json()); // req.body를 JSON으로 파싱
 //   res.send('hello world');
 // });
 
-// app.delete('/delete_board', async (req, res) => {
-//   try {
-//     const result = await pool.query('DELETE FROM board WHERE post_id = 1;;');
-//     return res.status(200).json({ message: 'Board Put Successfully' });
-//   } catch (error) {
-//     return res.status(500).json({ message: 'Update Completed Fail' + error });
-//   }
-// });
-
 app.use(require('./routes/getRoutes')); // getRoutes 사용
 app.use(require('./routes/postRoutes')); // postRoutes 사용
 app.use(require('./routes/putRoutes')); // updateRoutes 사용
